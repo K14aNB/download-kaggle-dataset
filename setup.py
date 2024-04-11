e07d6e06f72da0cdc2aeb87fb994b42ad84a1e06
K14aNB <kirnb@protonmail.com>
2024-04-11 10:58:18 +0530

    Fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 
 setup(
-    name='download_kaggle_datest',
+    name='download_kaggle_dataset',
     version='0.1.0',
     author='K14aNB',
     author_email='kirnid4@gmail.com',
