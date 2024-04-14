509ea0eb2bb2a044aa1692db44ac8cde96572be8
K14aNB <kirnb@protonmail.com>
2024-04-14 15:49:26 +0530

    Added default parameter nb_name to use in case of jupyter/.py script

--- download_kaggle_dataset.py
+++ download_kaggle_dataset.py
@@ -4,7 +4,7 @@ from importlib import import_module
 from subprocess import run,CalledProcessError
 from zipfile import ZipFile
 
-def download(data_src_path:str,colab=False,repo_path=None):
+def download(data_src_path:str,colab=False,repo_path=None,nb_name=None):
     '''
     Takes data_src_path in the form of <kaggle-username>/<dataset-name> 
     and downloads the dataset into colab/local runtime using kaggle api
@@ -15,8 +15,13 @@ def download(data_src_path:str,colab=False,repo_path=None):
     colab:bool : Indicates whether currently active environment uses colab runtime or local runtime.
                  default is False which indicates active environment is using local runtime.
     repo_path:str : Path of local repository to which dataset must be downloaded.
-                    default is None
+                    default value is None
                     This is applicable only for local runtime (when colab=False).
+    nb_name:str : Current Notebook name without the extensions (.ipynb,.py...).
+                  default value is None
+                  This is applicable only for local runtime (when colab=False)
+                  This value is needed so data can be downloaded and extracted in the 
+                  path:repo_path/data/nb_name/<data_file> when using local runtime 
     
     Returns:str : directory path where kaggle dataset is downloaded and extracted.
     '''
@@ -39,22 +44,33 @@ def download(data_src_path:str,colab=False,repo_path=None):
     dpath = ''
     if colab is True:
         dpath = os.getcwd()
+        if os.path.isdir(os.path.join(dpath,'data')) is False:
+            os.mkdir(os.path.join(dpath,'data'))
+        data_path=os.path.join(dpath,'data')
     else:
         dpath = repo_path
+        if nb_name is not None:
+            if os.path.isdir(os.path.join(dpath,'data',nb_name)) is False:
+                os.mkdir(os.path.join(dpath,'data',nb_name))
+            data_path=os.path.join(dpath,'data',nb_name)
+        else:
+            print('Notebook name is not provided. Default value is None')
+            raise FileNotFoundError(errno.ENOENT,os.strerror(errno.ENOENT),nb_name)
+    
 
-    os.mkdir(dpath+'/data')
-    try:
-        run(['python','-m','pip','install','kaggle'],check=True)
-    except CalledProcessError as e1:
-        print(f'{e1.cmd} failed')
-      
-    try:
-        run(['kaggle','datasets','download',data_src_path,'-p',os.path.join(dpath,'data')],check=True)
-    except CalledProcessError as e2:
-        print(f'{e2.cmd} failed')
+    if os.listdir(data_path) == []:
+        try:
+            run(['python','-m','pip','install','kaggle'],check=True)
+        except CalledProcessError as e1:
+            print(f'{e1.cmd} failed')
         
-    zip_filename = os.listdir(os.path.join(dpath,'data'))[0] 
-    with ZipFile(os.path.join(dpath,'data',zip_filename),'r') as zip:
-        zip.extractall(path=os.path.join(dpath,'data'))
-    print(f'Kaggle Dataset {data_src_path} is downloaded and extracted at {os.path.join(dpath,"data")}')
-    return os.path.join(dpath,'data')
+        try:
+            run(['kaggle','datasets','download',data_src_path,'-p',data_path],check=True)
+        except CalledProcessError as e2:
+            print(f'{e2.cmd} failed')
+            
+        zip_filename = os.listdir(data_path)[0] 
+        with ZipFile(os.path.join(data_path,zip_filename),'r') as zip:
+            zip.extractall(path=data_path)
+    print(f'Kaggle Dataset {data_src_path} is downloaded and extracted at {data_path}')
+    return data_path
